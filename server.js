f119b128bcc338fb83d2655a54f2557e5743f998
Bogdan Doncea <bogdan.cstrike@yahoo.com>
2020-11-03 23:26:33 +0200

    added env

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const dotenv = require('dotenv');
 const app = require('./app.js');
 const mongoose = require('mongoose');
 
-const port = 8000;
-
 mongoose
 	.connect(
 		'mongodb+srv://admin:admin@cluster0.ljnid.mongodb.net/<dbname>?retryWrites=true&w=majority',
@@ -18,6 +16,6 @@ mongoose
 		console.log('DB connection successful!');
 	});
 
-app.listen(process.env.port || port, () => {
+app.listen(process.env.PORT || 8000, () => {
 	console.log(`App running on port ${port}...`);
 });
